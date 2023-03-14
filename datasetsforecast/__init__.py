6e882c6ea9a22aa0a5e826c70ae46bd61dd0b2d2
Lee Joonhun <jhlee63@outlook.com>
2023-03-14 14:05:50 +0900

    Add direct importing

--- datasetsforecast/__init__.py
+++ datasetsforecast/__init__.py
@@ -1,1 +1,25 @@
+from .hierarchical import (
+    HierarchicalInfo,
+    Labour,
+    TourismLarge,
+    TourismSmall,
+    Traffic,
+    Wiki2,
+)
+from .long_horizon import (
+    ECL,
+    ILI,
+    ETTh1,
+    ETTh2,
+    ETTm1,
+    ETTm2,
+    Exchange,
+    LongHorizonInfo,
+    TrafficL,
+    Weather,
+)
+from .m3 import M3, M3Info
+from .m4 import M4, M4Evaluation, M4Info
+from .m5 import M5, M5Evaluation
+
 __version__ = "0.0.8"
