13d66b9a27abeb6edc289f071802228fdedf7d4c
deven367 <masterdeven@gmail.com>
2025-08-05 10:47:10 -0400

    current directory

--- tests/test_long_horizon2.py
+++ tests/test_long_horizon2.py
@@ -5,7 +5,7 @@ from datasetsforecast.long_horizon2 import LongHorizon2, LongHorizon2Info
 
 @pytest.mark.parametrize("group,meta", LongHorizon2Info)
 def test_longhorizon2(group, meta):
-    Y_df = LongHorizon2.load(directory='data', group=group)
+    Y_df = LongHorizon2.load(directory='./data', group=group)
     unique_elements = Y_df.groupby(['unique_id', 'ds']).size()
     unique_ts = Y_df.groupby('unique_id').size()
     n_time = len(Y_df.ds.unique())
